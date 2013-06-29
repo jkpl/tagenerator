88ce7fc1756dbb13d2e6fe9d502a19af9b18683b
Jaakko Pallari <jkpl@lepovirta.org>
2013-06-29 23:50:44 +0300

    Add TaAstParser for parsing Ast
    TaAstParser includes tools for parsing the abstract syntax tree
    generated by TaDocumentParser into custom types.

---
+++ src/TaGenerator/TaAstParser.hs
@@ -0,0 +1,52 @@
+module TaAstParser where
+
+import TaDocumentParser
+import Control.Applicative
+import qualified Data.Text as T
+import qualified Data.Map as M
+
+
+class FromAst a where
+  fromAst :: Ast -> Maybe a
+
+
+-- Basic instances
+
+instance FromAst Bool where
+  fromAst = boolConst
+
+instance FromAst T.Text where
+  fromAst = stringLiteral
+
+instance FromAst a => FromAst [a] where
+  fromAst = listFromAst
+
+-- Parsers
+
+boolConst :: Ast -> Maybe Bool
+boolConst (Variable v) = strToBool v
+boolConst _ = Nothing
+
+strToBool "yes" = Just True
+strToBool "no" = Just False
+
+stringLiteral :: Ast -> Maybe T.Text
+stringLiteral (StringLiteral s) = Just $ T.pack s
+stringLiteral _ = Nothing
+
+variable :: Ast -> Maybe String
+variable (Variable s) = Just s
+variable _ = Nothing
+
+listFromAst :: FromAst a => Ast -> Maybe [a]
+listFromAst (List l) = mapM fromAst l
+
+mapFromValueMap :: FromAst v => ValueMap -> Maybe (M.Map String v)
+mapFromValueMap vm = M.fromList <$> mapM from (M.toList vm)
+  where from (k,v) = (,) <$> Just k <*> fromAst v
+
+valueMap :: FromAst v => ValueMap -> M.Map String v
+valueMap = M.mapMaybe fromAst
+
+key :: FromAst a => String -> ValueMap -> Maybe a
+key k vm = M.lookup k vm >>= fromAst
