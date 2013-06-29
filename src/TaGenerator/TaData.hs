2a3402b575c8022acb25ced739521dce1a1e462f
Jaakko Pallari <jkpl@lepovirta.org>
2013-06-29 23:59:48 +0300

    Add parsers for TaData, tune data structures
    Fields that use Strings for string literals have been converted to Text
    type.
    
    Rooms don't have direct references to items or rooms. Instead, they are
    now collections of References. Each Reference includes one string
    identifier that should be the identifier for another data structure in
    the TextAdventure.
    
    Parsers use the ast parsing tools for parsing the abstract syntax tree.

--- src/TaGenerator/TaData.hs
+++ src/TaGenerator/TaData.hs
@@ -1,22 +1,94 @@
 module TaData where
 
+import Control.Applicative
+import TaDocumentParser
+import TaAstParser
+import qualified Data.Map as M
+import qualified Data.Text as T
 
-data TextAdventure = TextAdventure { startRoom :: Room
-                                   , rooms :: [Room]
-                                   , items :: [Item]
-                                      } deriving Show
-data Room = Room { roomName :: String
-                 , roomDescription :: String
-                 , roomItems :: [Item]
-                 , directions :: [Direction]
+
+type ReferenceMap = M.Map String Reference
+type RoomMap = M.Map String Room
+type ItemMap = M.Map String Item
+newtype Reference = Reference String deriving Show
+newtype DirectionMap = DirectionMap ReferenceMap deriving Show
+
+data TextAdventure = TextAdventure { startRoom :: Reference
+                                   , rooms :: RoomMap
+                                   , items :: ItemMap
+                                   } deriving Show
+data Room = Room { roomName :: T.Text
+                 , roomDescription :: T.Text
+                 , roomItems :: [Reference]
+                 , directions :: DirectionMap
                  } deriving Show
-data Direction = Direction String Room deriving Show
-data Item = Item { itemName :: String
-                 , itemDescription :: String
+data Item = Item { itemName :: T.Text
+                 , itemDescription :: T.Text
                  , itemActions :: [Action]
                  } deriving Show
 data Action = Action { actionType :: ActionType
                      , actionSuccess :: Bool
-                     , actionMessage :: String
+                     , actionMessage :: T.Text
                      } deriving Show
 data ActionType = PickUp | Look deriving Show
+
+
+instance FromAst Reference where
+  fromAst (Variable s) = Just $ Reference s
+  fromAst _ = Nothing
+
+instance FromAst Room where
+  fromAst = roomFromAst
+
+instance FromAst DirectionMap where
+  fromAst ast = DirectionMap <$> referenceMapFromAst ast
+
+instance FromAst Action where
+  fromAst = actionFromAst
+
+instance FromAst Item where
+  fromAst = itemFromAst
+
+
+referenceMapFromAst :: Ast -> Maybe ReferenceMap
+referenceMapFromAst (Block vm) = mapFromValueMap vm
+referenceMapFromAst _ = Nothing
+
+roomFromAst :: Ast -> Maybe Room
+roomFromAst (TypedBlock "room" vm) = valueMapToRoom vm
+roomFromAst _ = Nothing
+
+valueMapToRoom :: ValueMap -> Maybe Room
+valueMapToRoom vm = Room
+                    <$> key "name" vm
+                    <*> key "description" vm
+                    <*> key "items" vm
+                    <*> key "directions" vm
+
+actionFromAst :: Ast -> Maybe Action
+actionFromAst (TypedBlock t vm) = Action
+                                  <$> strToActionType t
+                                  <*> key "success" vm
+                                  <*> key "message" vm
+actionFromAst _ = Nothing
+
+strToActionType :: String -> Maybe ActionType
+strToActionType "pickup" = Just PickUp
+strToActionType "look" = Just Look
+strToActionType _ = Nothing
+
+itemFromAst :: Ast -> Maybe Item
+itemFromAst (TypedBlock "item" vm) = valueMapToItem vm
+itemFromAst _ = Nothing
+
+valueMapToItem :: ValueMap -> Maybe Item
+valueMapToItem vm = Item
+                    <$> key "name" vm
+                    <*> key "description" vm
+                    <*> key "actions" vm
+
+taFromValueMap :: ValueMap -> Maybe TextAdventure
+taFromValueMap vm = TextAdventure
+                    <$> key "start_room" vm
+                    <*> Just (valueMap vm)
+                    <*> Just (valueMap vm)
