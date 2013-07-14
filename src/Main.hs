2f406e4f74715d044aa4c5d410b98c2fbf64838a
Jaakko Pallari <jkpl@lepovirta.org>
2013-07-14 19:45:48 +0300

    Main program for "debugging" text adventure.
    Main program reads text adventure files from disk, parses and
    concatenates them, and then attempts to find broken references.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,39 @@
 module Main where
 
+import Data.Monoid
+import Control.Monad.Writer (runWriter)
+import System.IO
+import System.Directory
+import System.Environment (getArgs)
+import System.FilePath
+import TaGenerator.TaData
+import TaGenerator.TaCheck
+
+
 main :: IO ()
-main = putStrLn "cool story"
+main = do
+    args <- getArgs
+    dircontents <- getDirectoryFilePaths $ targetPath args
+    tas <- readAllTaFiles $ textAdventureFiles dircontents
+    print $ fmap (runWriter . checkTextAdventure) tas
+
+getDirectoryFilePaths :: FilePath -> IO [FilePath]
+getDirectoryFilePaths path = do
+    dircontents <- getDirectoryContents path
+    return $ fmap (combine path) dircontents
+
+targetPath :: [String] -> FilePath
+targetPath (x:_) = combine "." x
+targetPath [] = "."
+
+textAdventureFiles :: [FilePath] -> [FilePath]
+textAdventureFiles = filter ((==) ".ta" . takeExtension)
+
+readAllTaFiles :: [FilePath] -> IO (Maybe TextAdventure)
+readAllTaFiles files = fmap mconcat $ mapM readTaFromFile files
+
+readTaFromFile :: FilePath -> IO (Maybe TextAdventure)
+readTaFromFile fname = do
+    handle <- openFile fname ReadMode
+    contents <- hGetContents handle
+    return $ parseTextAdventure contents
