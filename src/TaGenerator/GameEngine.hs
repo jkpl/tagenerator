e313064833bc24f3cc3c97f80267cbefa29fa0c3
Jaakko Pallari <jkpl@lepovirta.org>
2013-08-08 14:39:47 +0300

    Implement "look at" action for game engine
    You can only look at items in the current room.

--- src/TaGenerator/GameEngine.hs
+++ src/TaGenerator/GameEngine.hs
@@ -6,7 +6,6 @@ import TaGenerator.TaData
 import qualified TaGenerator.CommandParser as C
 
 
-type ActionResult = (Bool, String)
 data GameState = GameOver | GameState GameData
 data GameData = GameData
                  { carrying :: [Item]
@@ -30,9 +29,9 @@ interact (GameState gamedata) command = interact' gamedata command
 
 interact' :: GameData -> C.Command -> (GameState, String)
 interact' gamedata command = case command of
-    (C.Go target) -> move gamedata target
-    (C.LookAt target) -> lookAt gamedata target
-    (C.PickUp target) -> pickUp gamedata target
+    C.Go target -> move gamedata target
+    C.LookAt target -> lookAt gamedata target
+    C.PickUp target -> pickUp gamedata target
     C.LookAround -> lookAround gamedata
 
 move :: GameData -> String -> (GameState, String)
@@ -41,8 +40,55 @@ move gd@(GameData inventory currentRoom ta) direction =
         Just nextRoom -> (gamestate inventory nextRoom ta, enterRoom nextRoom)
         Nothing -> (GameState gd, cantMove direction)
 
+enterRoom :: Room -> String
+enterRoom room = concat [ "Entered ", T.unpack (roomName room)
+                        , ": \n\n", T.unpack (roomDescription room)
+                        ]
+
+cantMove :: String -> String
+cantMove direction = "Can't move to " ++ direction
+
 lookAt :: GameData -> String -> (GameState, String)
-lookAt gd@(GameData inventory room ta) name = (GameState gd, "")
+lookAt gd name = (GameState gd, lookAt' gd name)
+
+lookAt' :: GameData -> String -> String
+lookAt' (GameData _ room ta) name = getStringFromEither $ do
+    (_, item) <- getItemFromRoom ta room name
+    doActionOnItem Look item
+
+getStringFromEither :: Either String String -> String
+getStringFromEither (Left s) = s
+getStringFromEither (Right s) = s
+
+getItemFromRoom :: TextAdventure
+                -> Room
+                -> String
+                -> Either String (String, Item)
+getItemFromRoom ta room itemName =
+    let item = itemFromRoom ta room itemName
+        failMessage = couldNotFind itemName
+    in maybeToEither failMessage item
+
+maybeToEither :: b -> Maybe a -> Either b a
+maybeToEither _ (Just a) = Right a
+maybeToEither b Nothing = Left b
+
+couldNotFind :: String -> String
+couldNotFind itemName = "Couldn't find " ++ itemName
+
+doActionOnItem :: ActionType -> Item -> Either String String
+doActionOnItem actiontype item = do
+    target <- listToEither failMessage actionList
+    return (T.unpack $ actionMessage target)
+  where
+    actionList = actionsFromItem item actiontype
+    failMessage = unknownAction actiontype (T.unpack $ itemName item)
+
+listToEither :: b -> [a] -> Either b a
+listToEither b = maybeToEither b . listToMaybe
+
+unknownAction :: ActionType -> String -> String
+unknownAction at s = concat ["Don't know how to ", (show at), " ", s, "."]
 
 pickUp :: GameData -> String -> (GameState, String)
 pickUp gd@(GameData inventory room ta) name = (GameState gd, "")
@@ -50,25 +96,3 @@ pickUp gd@(GameData inventory room ta) name = (GameState gd, "")
 lookAround :: GameData -> (GameState, String)
 lookAround gd = (GameState gd, description)
   where description = T.unpack . roomDescription . currentRoom $ gd
-
-doActionOnItem :: ActionType -> Item -> ActionResult
-doActionOnItem actiontype item = doForFirst action success failMessage
-  where action = actionsFromItem item actiontype
-        success a = (actionSuccess a, T.unpack $ actionMessage a)
-        failMessage = unknownAction actiontype (T.unpack $ itemName item)
-
-doForFirst :: [a] -> (a -> (Bool, String)) -> String -> (Bool, String)
-doForFirst targets f failMessage =
-    case listToMaybe targets of
-        Just firstTarget -> f firstTarget
-        Nothing -> (False, failMessage)
-
-unknownAction :: ActionType -> String -> String
-unknownAction at s = concat ["Don't know how to ", (show at), " ", s, "."]
-
-enterRoom :: Room -> String
-enterRoom room = concat [ "Entered ", T.unpack (roomName room)
-                        , ": \n\n", T.unpack (roomDescription room)]
-
-cantMove :: String -> String
-cantMove direction = "Can't move to " ++ direction
