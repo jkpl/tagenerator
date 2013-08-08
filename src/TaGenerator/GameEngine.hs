d734a1d406c2241de2c1fac45fc22991b37d7c84
Jaakko Pallari <jkpl@lepovirta.org>
2013-08-08 17:48:23 +0300

    Fix itemFromRoom and lookAround.
    itemFromRoom should now return the room that matches the name.
    lookAround should now display room description and room items. Did some
    additional refactoring.

--- src/TaGenerator/GameEngine.hs
+++ src/TaGenerator/GameEngine.hs
@@ -2,7 +2,7 @@ module TaGenerator.GameEngine where
 
 import qualified Data.Text as T
 import qualified Data.Map as M
-import Data.Maybe (listToMaybe)
+import Data.Maybe (listToMaybe, mapMaybe)
 import TaGenerator.TaData
 import qualified TaGenerator.CommandParser as C
 
@@ -22,7 +22,7 @@ initializeGameState ta = gamestate [] startroom ta
         roomOrFail Nothing = error "Could not find starting room."
 
 gamestate :: [Item] -> Room -> TextAdventure -> GameState
-gamestate items currentroom ta = GameState $ GameData items currentroom ta
+gamestate inventory currentroom ta = GameState $ GameData inventory currentroom ta
 
 interact :: GameState -> C.Command -> (GameState, String)
 interact GameOver _ = (GameOver, "Game Over")
@@ -36,14 +36,31 @@ interact' gamedata command = case command of
     C.LookAround -> lookAround gamedata
 
 move :: GameData -> String -> (GameState, String)
-move gd@(GameData inventory currentRoom ta) direction =
-    case roomAtDirection ta currentRoom direction of
-        Just nextRoom -> (gamestate inventory nextRoom ta, enterRoom nextRoom)
-        Nothing -> (GameState gd, cantMove direction)
+move gd direction = doGameStateTransition gd (move' direction)
 
-roomAtDirection :: TextAdventure -> Room -> String -> Maybe Room
-roomAtDirection ta room dir = getRoomRef >>= getRoom ta
-    where getRoomRef = M.lookup dir . directionMap . directions $ room
+doGameStateTransition :: GameData
+                      -> (GameData -> Either String (GameData, String))
+                      -> (GameState, String)
+doGameStateTransition gd f = case f gd of
+    Left s -> (GameState gd, s)
+    Right (newgd, s) -> (GameState newgd, s)
+
+move' :: String -> GameData -> Either String (GameData, String)
+move' direction (GameData inventory room ta) = do
+    nextRoom <- roomAtDirection ta room direction
+    return (GameData inventory nextRoom ta, enterRoom nextRoom)
+
+roomAtDirection :: TextAdventure -> Room -> String -> Either String Room
+roomAtDirection ta room direction =
+    maybeToEither (cantMove direction) (roomAtDirection' ta room direction)
+
+maybeToEither :: b -> Maybe a -> Either b a
+maybeToEither _ (Just a) = Right a
+maybeToEither b Nothing = Left b
+
+roomAtDirection' :: TextAdventure -> Room -> String -> Maybe Room
+roomAtDirection' ta room direction = getRoomRef >>= getRoom ta
+    where getRoomRef = M.lookup direction . directionMap . directions $ room
 
 enterRoom :: Room -> String
 enterRoom room = concat [ "Entered ", T.unpack (roomName room)
@@ -69,25 +86,33 @@ getItemFromRoom :: TextAdventure
                 -> Room
                 -> String
                 -> Either String (String, Item)
-getItemFromRoom ta room itemName =
-    let item = itemFromRoom ta room itemName
-        failMessage = couldNotFind itemName
+getItemFromRoom ta room name =
+    let item = itemFromRoom ta room name
+        failMessage = couldNotFind name
     in maybeToEither failMessage item
 
 itemFromRoom :: TextAdventure -> Room -> String -> Maybe (String, Item)
-itemFromRoom ta room itemName =
+itemFromRoom ta room name =
     let ris = map ref $ roomItems room
-    in firstFromMap $ M.filterWithKey (\k _ -> k `elem` ris) (items ta)
+        itemMap = items ta
+        itemsInRoom = M.filterWithKey (hasKeyInList ris) itemMap
+    in getFirstFromMap . filterItems $ itemsInRoom
+  where
+    hasKeyInList list k _ = k `elem` list
+    filterItems itemMap = filterItemMapWithName itemMap name
 
-firstFromMap :: M.Map k v -> Maybe (k, v)
-firstFromMap = listToMaybe . M.toList
+getFirstFromMap :: M.Map k v -> Maybe (k, v)
+getFirstFromMap m = listToMaybe $ M.toList m
 
-maybeToEither :: b -> Maybe a -> Either b a
-maybeToEither _ (Just a) = Right a
-maybeToEither b Nothing = Left b
+filterItemMapWithName :: ItemMap -> String -> ItemMap
+filterItemMapWithName itemMap name = M.filter matcher itemMap
+  where matcher item = itemHasName item name
+
+itemHasName :: Item -> String -> Bool
+itemHasName item name = (==) name . T.unpack . itemName $ item
 
 couldNotFind :: String -> String
-couldNotFind itemName = "Couldn't find " ++ itemName
+couldNotFind name = "Couldn't find " ++ name
 
 doActionOnItem :: ActionType -> Item -> Either String String
 doActionOnItem actiontype item = do
@@ -111,12 +136,19 @@ pickUp :: GameData -> String -> (GameState, String)
 pickUp gd@(GameData inventory room ta) name = (GameState gd, "")
 
 lookAround :: GameData -> (GameState, String)
-lookAround gd = (GameState gd, description)
+lookAround gd = (GameState gd, concat [description, "\n\n", showRoomItems gd])
   where description = T.unpack . roomDescription . currentRoom $ gd
 
-filterItemMapWithName :: ItemMap -> String -> ItemMap
-filterItemMapWithName items name = M.filter matcher items
-  where matcher item = itemHasName item name
+showRoomItems :: GameData -> String
+showRoomItems (GameData _ room ta) =
+    let itemsInRoom = getRoomItems ta room
+    in if null itemsInRoom
+       then "No items in this room."
+       else "Items in this room:\n" ++ showItemsAsList itemsInRoom
 
-itemHasName :: Item -> String -> Bool
-itemHasName item name = (==) name . T.unpack . itemName $ item
+showItemsAsList :: [Item] -> String
+showItemsAsList = concatMap renderItem
+  where renderItem item = concat ["- " , (T.unpack $ itemName item) , "\n"]
+
+getRoomItems :: TextAdventure -> Room -> [Item]
+getRoomItems ta room = mapMaybe (getItem ta) (roomItems room)
